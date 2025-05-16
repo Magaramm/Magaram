c91058c2ecefa5f3ca64506265a1b1ca140e208d
Magaramm <gdfg9916@gmail.com>
2025-05-17 04:34:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def home():
 
 def run_web():
     port = int(os.environ.get('PORT', 10000)) 
-    app.run(host='0.0.0.0', port=port))
+    app.run(host='0.0.0.0', port=port)
 
 Thread(target=run_web).start()
 
